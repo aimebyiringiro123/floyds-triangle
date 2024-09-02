f1d2efdb7bc3b0b9040b11ad7bb58aa216031009
Arson <143195328+aimebyiringiro123@users.noreply.github.com>
2024-09-02 19:07:54 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@floyds-triangle/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int i, j, rows, n= 1;
+  printf("enter the number of rows ");
+  scanf("%d", &rows);
+
+  for(i= 1; i<= rows; i++){
+    for(j= 1; j<= i; j++){
+      printf("%d", n);
+      n++;
+    }
+    printf("\n");
+  }
+
+  
+  return 0;
+}
